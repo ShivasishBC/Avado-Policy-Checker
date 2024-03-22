b3c57470b15536fa698b148ed0f13e945741b479
ShivasishBC <139316256+ShivasishBC@users.noreply.github.com>
2024-03-22 15:20:28 +0530

    Update HR_policy_checker_avado.py

--- HR_policy_checker_avado.py
+++ HR_policy_checker_avado.py
@@ -7,7 +7,7 @@ def get_openai_client(api_key, endpoint):
     openai.api_version = '2023-12-01-preview'
     openai.api_base = endpoint
     return openai
-
+    
 def gpt_function(client, hr_policy, sector, country):
     """
     Function to call the GPT API and return a response.
@@ -18,23 +18,20 @@ def gpt_function(client, hr_policy, sector, country):
     Country: {country}
     
     Use the provided company policy, sector, and country information to ensure alignment with legal standards and best practices.
-    Provide a report highlighting alignment with industry best practices, including a summary of country best practices, alignment of your policy with regional best practices, and recommendations for greater alignment.
-    The response must follow british english 
+    Provide a report highlighting alignment with industry best practices, including a summary of country best practices, alignment of your policy with regional best practices, and recommendations for greater alignment in british english.
 """
 
     conversation = [
-        {"role": "system", "content": """
-                    You are a Policy Proofer bot 
+        {"role": "system", "content": """You are a Policy Proofer bot 
                         
                         Few details about the <Company name>, employees who stay within 50 miles radius of the office have to work from the office; others can leverage WFH.
                         If a woman has to work after 8 PM, she will be given cab service to her home.
                         If you are working on holidays, then 1 compensation leave and a bonus will be added to your salary at the end of the month.
     
-                    You will be given the following information with proper structured format and subheadings  including Company Sector (Sector) , Country Located (Country):
+                        You will be given the following information with proper format and subheadings including Company Sector (Sector) , Country Located (Country):
                             1. ### Summary of region specific best practices 
                             2. ### How Your HR Policy compares 
                             3. ### Recomandations to improve your policy
-                    
                                     """},
         {"role": "user", "content": f"{user_content}"}
     ]
@@ -43,6 +40,7 @@ def gpt_function(client, hr_policy, sector, country):
         messages=conversation,
         engine="gpt-35-turbo",
         temperature = 0
+
     )
     text_response = response.choices[0].message.content
     return text_response
